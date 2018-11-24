e3e9624cac76815c23f3f89ae61f5c52aabec246
Quique Fdez Guerra <quique@ckgrafico.com>
2018-11-24 16:30:15 +0100

    Add basic things

--- src/app/bingo/Bingo.vue
+++ src/app/bingo/Bingo.vue
@@ -1,19 +1,33 @@
 <template>
   <section class="bingo">
-    Hello!!
+    <img class="bingo-logo" src="/assets/images/logo.png"/>
+    <div class="bingo-list">
+      <Number v-for="number in numberValues" :key="number" :value="number + 1" />
+    </div>
   </section>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-
+import Number from './Number.vue';
 
 @Component({
   components: {
+    Number
   }
 })
 export default class extends Vue {
   public name = 'Bingo';
+  public numbersCount = 90;
+  public numberValues = [];
+
+  public created(): void {
+    this.initBingo();
+  }
+
+  private initBingo(): void {
+    this.numberValues = [...Array(this.numbersCount).keys()]
+  }
 }
 </script>
 
@@ -21,10 +35,18 @@ export default class extends Vue {
 @import '~styles/variables';
 
 .bingo {
-  align-content: center;
-  align-items: center;
-  display: flex;
-  justify-content: center;
   height: 100vh;
+
+  &-logo {
+    margin-left: 2rem;
+  }
+
+  &-list {
+    align-content: flex-start;
+    align-items: flex-start;
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+  }
 }
 </style>
---
+++ src/app/bingo/Number.vue
@@ -0,0 +1,49 @@
+<template>
+  <section class="number" @click="toggle" :class="{'is-on': state}">
+    {{value}}
+  </section>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator';
+
+
+@Component({
+  components: {
+  }
+})
+export default class extends Vue {
+  public name = 'Number';
+  public state = false;
+
+  @Prop({ type: Number, required: true }) value: number;
+
+  public toggle(): void {
+    this.state = !this.state;
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '~styles/variables';
+
+.number {
+  $size: 6rem;
+
+  align-items: center;
+  border-radius: 50%;
+  cursor: pointer;
+  display: flex;
+  font-size: $size * .5;
+  height: $size;
+  margin: .25rem;
+  justify-content: center;
+  opacity: .15;
+  width: $size;
+
+  &.is-on {
+    background-color: $color-primary;
+    opacity: 1;
+  }
+}
+</style>
